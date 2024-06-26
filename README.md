1049bb8ddf26d562e7792adaec4fa8998ab41475
YaNesyTortiK <skripter35@yandex.ru>
2024-06-26 12:55:45 +0300

    Initialization and testing

---
+++ README.md
@@ -0,0 +1,7 @@
+# Example Package
+
+This is a simple example package. You can use
+[GitHub-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
+
+Testing
---
+++ src/anime-parsers-ru/parsers.py
@@ -0,0 +1,10 @@
+class Test:
+    """
+    # Test package
+    Does literally nothing
+    """
+    def __init__(self) -> None:
+        """
+        ## Just to test
+        """
+        print('Testing package')
