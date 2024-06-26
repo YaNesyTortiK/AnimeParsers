9d77661b2fe86ba2cc2380c54f30d89ff9e0ba05
YaNesyTortiK <skripter35@yandex.ru>
2024-06-26 20:26:38 +0300

    Kodik parser done; basic docs done, build 1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## To 1.0.0
+- Added Kodik parser
+    * Auto grabbing api key
---
+++ src/anime_parsers_ru/errors.py
@@ -0,0 +1,24 @@
+class TokenError(Exception):
+    """
+    Ошибка для обозначения неверного токена.
+    """
+
+class ServiceError(Exception):
+    """
+    Ошибка для обозначения ошибки на стороне сервера
+    """
+
+class PostArgumentsError(Exception):
+    """
+    Ошибка для обозначения неверно переданных аргументов серверу
+    """
+
+class NoResults(Exception):
+    """
+    Ошибка для обозначения отсутствия результатов
+    """
+
+class UnexpectedBehaviour(Exception):
+    """
+    Ошибка для обозначения неожиданного или необработанного поведения
+    """
